4d5580ee4b556fa8980cfb1b5056183d205ad28f
MatiasRojasLemus <149407312+MatiasRojasLemus@users.noreply.github.com>
2024-11-19 20:11:15 +0100

    Definido el movimiento y la capacidad de salto del objeto Player

--- ProyectoUnity3DMatiasRojasLemus/Assets/Scripts/PlayerController.cs
+++ ProyectoUnity3DMatiasRojasLemus/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speedMove = 10f;
+    public float jumpPower = 7f;
+    public CharacterController playerController;
+    public float gravityScale = 1f;
+    
+    
+    private Vector3 moveDirection;
+    private Vector3 jumpDirection;
+    private float yStore;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        yStore = moveDirection.y;
+
+        //Movimiento
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveDirection = moveDirection * speedMove;
+
+
+        moveDirection.y = yStore;
+
+
+        //Salto
+        if (Input.GetButton("Jump"))
+        {
+            moveDirection.y = jumpPower;
+        }
+
+        moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
+        playerController.Move(moveDirection * Time.deltaTime);
     }
 }
