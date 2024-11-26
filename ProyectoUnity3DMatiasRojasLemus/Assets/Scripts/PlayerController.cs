769c3bba90eb2bbda92f5137d599360f279b79a3
MatiasRojasLemus <149407312+MatiasRojasLemus@users.noreply.github.com>
2024-11-26 16:33:28 +0100

    Player: Implementadas las animaciones y transiciones de los estados Idle, salto, y correr

--- ProyectoUnity3DMatiasRojasLemus/Assets/Scripts/PlayerController.cs
+++ ProyectoUnity3DMatiasRojasLemus/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,10 @@ public class PlayerController : MonoBehaviour
 {
     public float speedMove = 10f;
     public float jumpPower = 7f;
-    public CharacterController playerController;
     public float gravityScale = 1f;
+    public CharacterController playerController;
     public Camera playerCamera;
+    public Animator animator;
     
     
     private Vector3 moveDirection;
@@ -22,25 +23,70 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        checkMovement();
+    }
+
+    void checkMovement()
+    {
         yStore = moveDirection.y;
 
         //Movimiento
-        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        moveDirection = moveDirection * speedMove;
-
+        checkMove();
 
         moveDirection.y = yStore;
 
-
         //Salto
-        if (Input.GetButton("Jump"))
-        {
-            moveDirection.y = jumpPower;
-        }
+        checkJump();
 
         moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
         playerController.Move(moveDirection * Time.deltaTime);
 
+        //Rotación del personaje
+        playerRotation();
+
+        //Deteccion de suelo
+        checkGround();
+    }
+
+    void playerRotation()
+    {
         transform.rotation = Quaternion.Euler(0f, playerCamera.transform.rotation.eulerAngles.y, 0f);
     }
+
+    void checkJump()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            moveDirection.y = jumpPower;
+        }
+    }
+
+    void checkMove()
+    {
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveDirection = moveDirection * speedMove;
+
+        if ((Mathf.Abs(playerController.velocity.x) != 0) || (Mathf.Abs(playerController.velocity.z) != 0))
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+    }
+
+    void checkGround()
+    {
+        if (playerController.isGrounded)
+        {
+            animator.SetBool("isGrounded", true);
+        }
+        else
+        {
+            animator.SetBool("isGrounded", false);
+        }
+    }
+
+
 }
